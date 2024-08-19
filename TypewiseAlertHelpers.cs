6269b3ba980005e2ede07c2f829079e83cdb0ee4
prabhatkapse <prabhatkapse@gmail.com>
2024-08-19 19:18:50 +0530

    Update TypewiseAlertHelpers.cs

--- TypewiseAlertHelpers.cs
+++ TypewiseAlertHelpers.cs
@@ -1,31 +1,32 @@
-using BatteryAlert;
-
-internal static class TypewiseAlertHelpers
+namespace BatteryAlert
 {
-    private static readonly Dictionary<CoolingType, (int lowerLimit, int upperLimit)> CoolingLimits = new Dictionary<CoolingType, (int lowerLimit, int upperLimit)>
+    public static class TypewiseAlertHelpers
     {
-        { CoolingType.PASSIVE_COOLING, (0, 35) },
-        { CoolingType.HI_ACTIVE_COOLING, (0, 45) },
-        { CoolingType.MED_ACTIVE_COOLING, (0, 40) }
-    };
-
-    public static BreachType ClassifyTemperatureBreach(CoolingType coolingType, double temperatureInC)
-    {
-        var limits = CoolingLimits[coolingType];
-        return InferBreach(temperatureInC, limits.lowerLimit, limits.upperLimit);
-    }
-
-
-    public static BreachType InferBreach(double value, double lowerLimit, double upperLimit)
-    {
-        if (value < lowerLimit)
+        private static readonly Dictionary<CoolingType, (int lowerLimit, int upperLimit)> CoolingLimits = new Dictionary<CoolingType, (int lowerLimit, int upperLimit)>
         {
-            return BreachType.TOO_LOW;
-        }
-        if (value > upperLimit)
+            { CoolingType.PASSIVE_COOLING, (0, 35) },
+            { CoolingType.HI_ACTIVE_COOLING, (0, 45) },
+            { CoolingType.MED_ACTIVE_COOLING, (0, 40) }
+        };
+
+        public static BreachType ClassifyTemperatureBreach(CoolingType coolingType, double temperatureInC)
         {
-            return BreachType.TOO_HIGH;
+            var limits = CoolingLimits[coolingType];
+            return InferBreach(temperatureInC, limits.lowerLimit, limits.upperLimit);
         }
-        return BreachType.NORMAL;
+
+
+        public static BreachType InferBreach(double value, double lowerLimit, double upperLimit)
+        {
+            if (value < lowerLimit)
+            {
+                return BreachType.TOO_LOW;
+            }
+            if (value > upperLimit)
+            {
+                return BreachType.TOO_HIGH;
+            }
+            return BreachType.NORMAL;
+        }
     }
 }
